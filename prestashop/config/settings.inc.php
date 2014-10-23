d5c3e79c54c6c34ba174271635e297b94b041ee5
MarionGeorges <marion.georges@yahoo.fr>
2014-10-23 16:12:18 +0200

    panier OK -> Bar header OK
    Next : newsletter en face du logo, et pb JS du menu.

--- prestashop/config/settings.inc.php
+++ prestashop/config/settings.inc.php
@@ -1,6 +1,6 @@
 <?php
 define('_DB_SERVER_', 'localhost');
-define('_DB_NAME_', 'PS_EdUnd');
+define('_DB_NAME_', 'PS_EdUnd2');
 define('_DB_USER_', 'root');
 define('_DB_PASSWD_', 'root');
 define('_DB_PREFIX_', 'ps_');
